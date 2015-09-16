a63a25591bb650aac3f5cb95dd9c1a4befde9680
Luis Echegaray <mrluisechegaray@gmail.com>
2015-09-15 21:29:42 -0400

    progress on gulp course

--- gulp.config.js
+++ gulp.config.js
@@ -1,12 +1,12 @@
 module.exports = function () {
     var config = {
-        
+
         //all the js I vet
         alljs: [
             './src/**/*.js',
             './*.js'
         ]
     };
-    
+
     return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,14 @@
-/*jslint node:true */
-(function () {
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
-    'use strict';
-
-    var gulp = require('gulp'),
-        args = require('yargs').argv,
-        config = require('./gulp.config')(),
-        $ = require('gulp-load-plugins')({lazy: true});
-
-    gulp.task('vet', function () {
-        log('Analyzing source with JSHint and JSCS');
-        
-        gulp.src(config.alljs)
-            .pipe($.if(args.verbose, $.print()))
-            .pipe($.jscs())
-            .pipe($.jshint())
-            .pipe($.jshint.reporter('jshint-stylish'), {verbose: true})
-            .pipe($.jshint.reporter('fail'));
-
-    });
-    
-    function log(msg) {
-        var item;
-        if (typeof (msg) === 'object') {
-            for (item in msg) {
-                if (msg.hasOwnProperty(item)) {
-                    $.util.log($.util.colors.blue(msg[item]));
-                }
-            }
-        } else {
-            $.util.log($.util.colors.blue(msg));
-        }
-                    
-    }
-
-}());
+gulp.task('vet', function() {
+    return gulp
+        .src([
+            './src/**/*.js',
+            './*.js'
+        ])
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
+});
